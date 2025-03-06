04f3b67836d96518ff1c373ca47a50968cc5c055
vardhan411205 <vijjijovardhan@gmail.com>
2025-03-06 09:55:01 +0530

    f6

--- joo/routers.py
+++ joo/routers.py
@@ -4,45 +4,42 @@ class DatabaseRouter:
     """
     def db_for_read(self, model, **hints):
         """
-        Attempts to read auth and contenttypes models go to default db.
-        All other models go to items db.
+        Route read operations to appropriate database
         """
         if model._meta.app_label in ['auth', 'contenttypes', 'sessions', 'admin']:
             return 'default'
-        if model._meta.model_name in ['venuepartner', 'restaurantpartner', 'fooditem', 'diningtable', 'deliverypartner', 'otpverification', 'resetpassword']:  # Partner, auth, and related models go to default
+        if model._meta.model_name in ['venuepartner', 'restaurantpartner', 'fooditem', 'diningtable', 'deliverypartner', 'otpverification', 'resetpassword']:
             return 'default'
-        if model._meta.model_name == 'order' or model._meta.app_label == 'joo':  # Order model and other joo models go to items
+        if model._meta.model_name == 'order':
             return 'items'
         return 'default'
 
     def db_for_write(self, model, **hints):
         """
-        Attempts to write auth and contenttypes models go to default db.
-        All other models go to items db.
+        Route write operations to appropriate database
         """
         if model._meta.app_label in ['auth', 'contenttypes', 'sessions', 'admin']:
             return 'default'
-        if model._meta.model_name in ['venuepartner', 'restaurantpartner', 'fooditem', 'diningtable', 'deliverypartner', 'otpverification', 'resetpassword']:  # Partner, auth, and related models go to default
+        if model._meta.model_name in ['venuepartner', 'restaurantpartner', 'fooditem', 'diningtable', 'deliverypartner', 'otpverification', 'resetpassword']:
             return 'default'
-        if model._meta.model_name == 'order' or model._meta.app_label == 'joo':  # Order model and other joo models go to items
+        if model._meta.model_name == 'order':
             return 'items'
         return 'default'
 
     def allow_relation(self, obj1, obj2, **hints):
         """
-        Allow relations between objects in the same database.
+        Allow relations between objects in the same database
         """
         return True
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         """
-        Make sure the auth and contenttypes apps only appear in the
-        'default' database.
+        Control which migrations go to which database
         """
         if app_label in ['auth', 'contenttypes', 'sessions', 'admin']:
             return db == 'default'
-        if model_name in ['venuepartner', 'restaurantpartner', 'fooditem', 'diningtable', 'deliverypartner', 'otpverification', 'resetpassword']:  # Partner, auth, and related migrations go to default
+        if model_name in ['venuepartner', 'restaurantpartner', 'fooditem', 'diningtable', 'deliverypartner', 'otpverification', 'resetpassword']:
             return db == 'default'
-        if model_name == 'order' or app_label == 'joo':  # Order migrations and other joo migrations go to items
+        if model_name == 'order':
             return db == 'items'
         return db == 'default' 
